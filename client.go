7667fa1b353d0f6d49c2258548ec7a62e16ef10a
Arsham <arshamdev2001@gmail.com>
2024-08-27 19:54:13 +0330

    add:client based on cycletls & post and get method

--- client.go
+++ client.go
@@ -1,20 +1,54 @@
 package cf_forbidden
 
 import (
-	"crypto/tls"
+	"github.com/Danny-Dasilva/CycleTLS/cycletls"
 	"net/http"
 )
 
-func New(browser Browser, debug bool) *http.Client {
-	tlsConfig := &tls.Config{
-		CipherSuites:       cipherSuites[browser],
-		MinVersion:         tls.VersionTLS12,
-		MaxVersion:         tls.VersionTLS13,
-		InsecureSkipVerify: debug,
+type M map[string]string
+
+type Client struct {
+	tls     cycletls.CycleTLS
+	headers M
+	ja3     string
+	agent   string
+}
+
+func New() (*Client, error) {
+	client := &Client{
+		tls: cycletls.Init(),
 	}
-	return &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
-		},
+	return client, nil
+}
+
+func (c Client) Do(url string, opt cycletls.Options, method string) (cycletls.Response, error) {
+	for k, v := range c.headers {
+		opt.Headers[k] = v
 	}
+	if opt.UserAgent == "" {
+		opt.UserAgent = c.agent
+	}
+	if opt.Ja3 == "" {
+		opt.Ja3 = c.ja3
+	}
+	return c.tls.Do(url, opt, method)
+}
+
+func (c Client) Get(url string, headers M) (cycletls.Response, error) {
+	opt := cycletls.Options{
+		UserAgent: c.agent,
+		Headers:   headers,
+		Ja3:       c.ja3,
+	}
+	return c.Do(url, opt, http.MethodGet)
+}
+
+func (c Client) Post(url string, headers M, body string) (cycletls.Response, error) {
+	options := cycletls.Options{
+		Ja3:       c.ja3,
+		Body:      body,
+		UserAgent: c.agent,
+		Headers:   headers,
+	}
+	return c.Do(url, options, http.MethodPost)
 }
